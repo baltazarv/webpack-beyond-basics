0e9dd462f82032df5788138a7337abae8802fcc1
Baltazar Villegas <baltazarv@gmail.com>
2019-09-17 00:37:17 -0400

    Added source-maps and the nodemon --inspect flag that allows for server debugging on Node DevTools.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 require('babel-runtime/regenerator')
 require('webpack-hot-middleware/client?reload=true')
 require('./main.css')
-require('./images/river-phoenix.jpg')
 require('./index.html')
+// debugger
