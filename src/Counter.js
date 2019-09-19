3af4d8ffb1eb4dbdebc287ed316ca7aba07f1cbe
Baltazar Villegas <baltazarv@gmail.com>
2019-09-19 00:02:24 -0400

    Using CSS  in JS: two ways (1) using modules, which are options on the css-loader itself, or (2) using emotion and @emotion/styled packages.

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,21 @@
 /* eslint-disable react/display-name */
 import React from 'react'
+import styles from './main.css'
+// import styled from 'react-emotion'
+import styled from '@emotion/styled'
+import { css } from 'emotion'
+
+const counterColor = 'salmon'
+const counterStyles = css`
+	color: ${counterColor};
+	font-size: 2em;
+`
+const Fancy = styled.div`
+	color: ${props => (props.wild ? 'hotpink' : 'gold')};
+	font-size: 3em;
+`
+
+import riverImg from './images/river-phoenix.jpg'
 
 export default class extends React.Component {
 	constructor(props) {
@@ -16,9 +32,16 @@ export default class extends React.Component {
 	}
 
 	render() {
+		const isWild = this.state.count % 2 === 0
 		return (
-			<div onClick={this.climb.bind(this)}>
-				<h1>Count: {this.state.count}</h1>
+			<div className={styles.profile}>
+				<img src={riverImg} alt="River Phoenix" />
+				<h1 className={styles.h1}>Hello, River</h1>
+				<div
+				className={styles.counter}
+				onClick={this.climb.bind(this)}>
+					<Fancy wild={isWild}>Count: {this.state.count}</Fancy>
+				</div>
 			</div>
 		)
 	}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ require('babel-runtime/regenerator')
 require('react-hot-loader/patch')
 require('babel-register')
 require('webpack-hot-middleware/client?reload=true')
-require('./main.css')
 require('./index.html')
 require('./App')
 // debugger
